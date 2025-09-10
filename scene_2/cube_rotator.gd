1c4f89a0c8bb02bacee354d6186e2d3d0e3ac00a
ISAKUTOKI <isakutoki@gmail.com>
2025-09-10 22:24:46 +0900

    2025/09/10
    重新梳理了旋转的逻辑，计划重构代码，把功能模块化（只是计划）

---
+++ scene_2/cube_rotator.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@onready var main_camera: Camera3D = $"../MainCamera"
+@onready var cube: Cube = $"../Cube"
--- scene_2/main_camera.gd
+++ scene_2/main_camera.gd
@@ -10,10 +10,10 @@ var outline_material = preload("res://scene_2/cube_outline_material.tres")
 
 func _process(_delta: float) -> void:
 	_outline()
-	
 
 
 func _input(event: InputEvent) -> void:
+	# 判断目标
 	if event is InputEventMouseMotion:
 		var mouse_pos = event.position
 		# 射线
@@ -28,8 +28,15 @@ func _input(event: InputEvent) -> void:
 		else:
 			target = null
 
+	# 旋转cube
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("rotate_cube"):
+			_rotate_cube()
+	# 旋转cubie
 	if event is InputEventMouseButton and target:
-		pass
+		if event.is_action_pressed("rotate_cubie"):
+			_rotate_cubie()
+
 
 func _outline():
 	if last_target != target:
@@ -38,3 +45,27 @@ func _outline():
 		last_target = target
 		if target:
 			target.material_override = outline_material
+
+
+func _rotate_cube():
+	var dir = _get_mouse_direction()
+
+
+func _rotate_cubie():
+	var dir = _get_mouse_direction()
+	var normal = _get_cubie_normal()
+	var index = _get_rotate_index(dir)
+
+
+func _get_mouse_direction() -> Vector2:
+	var dir: Vector2
+	return dir
+
+
+func _get_cubie_normal():
+	pass
+
+
+func _get_rotate_index(direction: Vector2) -> Cube.RotateIndex:
+	var ind: Cube.RotateIndex
+	return ind
